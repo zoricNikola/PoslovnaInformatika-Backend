d2f809a42d2692ac0d0081fb220426d26d902540
Nikola Zoric <nikola.se.zoric@gmail.com>
2021-08-18 19:13:07 +0200

    Created spring boot project

---
+++ narodna-banka/src/test/java/com/ftn/poslovnainformatika/narodnabanka/NarodnaBankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.poslovnainformatika.narodnabanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarodnaBankaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
