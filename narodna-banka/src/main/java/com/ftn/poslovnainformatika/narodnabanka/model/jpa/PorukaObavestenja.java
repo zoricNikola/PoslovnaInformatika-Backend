ea5397fe99fc2de7c47bcff249c523583bcbb99a
Boris <jankovicb0230@gmail.com>
2021-08-25 18:15:09 +0200

    Kreirani entiteti Racun, SifarnikDelatnosti i PorukaObavestenja

---
+++ narodna-banka/src/main/java/com/ftn/poslovnainformatika/narodnabanka/model/jpa/PorukaObavestenja.java
@@ -0,0 +1,46 @@
+package com.ftn.poslovnainformatika.narodnabanka.model.jpa;
+
+import com.ftn.poslovnainformatika.narodnabanka.model.jpa.poslovnabanka.PoslovnaBanka;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "poruka_obavestenja")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PorukaObavestenja {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "poruka_obavestenja_id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "vrsta_poruke")
+    private VrstaPoruke vrstaPoruke;
+
+    @Column(name = "swift_kod")
+    private String swiftKod;
+
+    @Column(name = "obracunski_racun")
+    private String obracunskiRacun;
+
+    @Column(name = "datum_valute")
+    private LocalDate datumValute;
+
+    @Column(name = "sifra_valute")
+    private String sifraValute;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "poruka", referencedColumnName = "poruka_id", nullable = false)
+    private Poruka poruka;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "poslovna_banka", referencedColumnName = "sifra_banke", nullable = false)
+    private PoslovnaBanka poslovnaBanka;
+}
---
+++ narodna-banka/src/main/java/com/ftn/poslovnainformatika/narodnabanka/model/jpa/SifarnikDelatnosti.java
@@ -0,0 +1,31 @@
+package com.ftn.poslovnainformatika.narodnabanka.model.jpa;
+
+import com.ftn.poslovnainformatika.narodnabanka.model.jpa.poslovnabanka.Klijent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sifarnik_delatnosti")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SifarnikDelatnosti {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sifarnik_delatnosti_id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "sifra_delatnosti")
+    private double sifraDelatnosti;
+
+    @Column(name = "naziv_delatnosti", nullable = false)
+    private String nazivDelatnosti;
+
+    //@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "klijent")
+    private Klijent klijent;
+}
