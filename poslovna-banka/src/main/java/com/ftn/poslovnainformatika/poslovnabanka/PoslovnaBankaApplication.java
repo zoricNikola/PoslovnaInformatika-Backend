787c80d2d349bb07fa6dfcb16e3acc7d0c4b0a35
Tijana <rosictijana95@gmail.com>
2021-09-03 14:47:39 +0200

    Kreiran dodatni projekat poslovna-banka

---
+++ poslovna-banka/src/main/java/com/ftn/poslovnainformatika/poslovnabanka/PoslovnaBankaApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.poslovnainformatika.poslovnabanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaBankaApplication.class, args);
+	}
+
+}
---
+++ poslovna-banka/src/test/java/com/ftn/poslovnainformatika/poslovnabanka/PoslovnaBankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.poslovnainformatika.poslovnabanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslovnaBankaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
