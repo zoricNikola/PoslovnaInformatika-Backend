65fd27943a082e5667533851825ee26464a06e6a
Boris <jankovicb0230@gmail.com>
2021-09-04 15:05:48 +0200

    Kreiran servis za delatnosti

---
+++ narodna-banka/src/main/java/com/ftn/poslovnainformatika/narodnabanka/service/impl/poslovnabanka/DelatnostService.java
@@ -0,0 +1,58 @@
+package com.ftn.poslovnainformatika.narodnabanka.service.impl.poslovnabanka;
+
+import com.ftn.poslovnainformatika.narodnabanka.converter.DtoConverter;
+import com.ftn.poslovnainformatika.narodnabanka.dto.poslovnabanka.DelatnostDTO;
+import com.ftn.poslovnainformatika.narodnabanka.model.jpa.poslovnabanka.Delatnost;
+import com.ftn.poslovnainformatika.narodnabanka.repository.poslovnabanka.DelatnostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Set;
+
+public class DelatnostService implements com.ftn.poslovnainformatika.narodnabanka.service.poslovnabanka.DelatnostService {
+    @Autowired
+    private DtoConverter<Delatnost, DelatnostDTO> delatnostConverter;
+
+    @Autowired
+    private DelatnostRepository delatnostRepository;
+
+    @Override
+    public DelatnostDTO getOne(int id) {
+        Delatnost delatnost = delatnostRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+
+        return delatnostConverter.convertToDTO(delatnost);
+    }
+
+    @Override
+    public int create(DelatnostDTO delatnostDTO) {
+        Delatnost delatnost = delatnostConverter.convertToJPA(delatnostDTO);
+
+        delatnost = delatnostRepository.save(delatnost);
+
+        return delatnost.getId();
+    }
+
+    @Override
+    public void update(int id, DelatnostDTO delatnostDTO) {
+        Delatnost delatnost = delatnostRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+
+        delatnost.setNazivDelatnosti(delatnostDTO.getNazivDelatnosti());
+        delatnost.setSifraDelatnosti(delatnostDTO.getSifraDelatnosti());
+
+        delatnostRepository.save(delatnost);
+    }
+
+    @Override
+    public void delete(int id) {
+        Delatnost delatnost = delatnostRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+
+        delatnostRepository.deleteById(id);
+    }
+
+    public Set<DelatnostDTO> getAll(){
+        List<Delatnost> delatnosti = delatnostRepository.findAll();
+
+        return delatnostConverter.convertToDTO((Set<Delatnost>) delatnosti);
+    }
+}
---
+++ narodna-banka/src/main/java/com/ftn/poslovnainformatika/narodnabanka/service/poslovnabanka/DelatnostService.java
@@ -0,0 +1,7 @@
+package com.ftn.poslovnainformatika.narodnabanka.service.poslovnabanka;
+
+import com.ftn.poslovnainformatika.narodnabanka.dto.poslovnabanka.DelatnostDTO;
+import com.ftn.poslovnainformatika.narodnabanka.service.common.BaseService;
+
+public interface DelatnostService extends BaseService<DelatnostDTO> {
+}
