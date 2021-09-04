d8e4139032905d0dbd556b330f42b62395c26bd2
Boris <jankovicb0230@gmail.com>
2021-09-04 15:20:06 +0200

    Kreiran servis za obavestenja

---
+++ narodna-banka/src/main/java/com/ftn/poslovnainformatika/narodnabanka/service/ObavestenjeService.java
@@ -0,0 +1,7 @@
+package com.ftn.poslovnainformatika.narodnabanka.service;
+
+import com.ftn.poslovnainformatika.narodnabanka.dto.ObavestenjeDTO;
+import com.ftn.poslovnainformatika.narodnabanka.service.common.BaseService;
+
+public interface ObavestenjeService extends BaseService<ObavestenjeDTO> {
+}
